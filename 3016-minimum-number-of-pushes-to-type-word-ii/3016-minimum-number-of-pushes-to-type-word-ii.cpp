5e4476ce3c01f637e136667e8d74f52f58f5c23e
Dhumil Kachhiya <140485981+Dhumilkachhiya@users.noreply.github.com>
2026-07-31 11:37:43 +0530

    Time: 11 ms (48.07%), Space: 28.5 MB (15.83%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n = word.size();
         vector<int> freq(26, 0);
-        for (char c : word) {
+
+        for (char c : word)
             freq[c - 'a']++;
-        }
+
         sort(freq.rbegin(), freq.rend());
 
-        int m = freq.size();
         int ans = 0;
-        int miin = min(8, m);
-        for (int i = 0; i < miin; i++) {
-            ans += freq[i];
+
+        for (int i = 0; i < 26; i++) {
+            if (i < 8)
+                ans += freq[i];
+            else if (i < 16)
+                ans += freq[i] * 2;
+            else if (i < 24)
+                ans += freq[i] * 3;
+            else
+                ans += freq[i] * 4;
         }
-        miin = min(16, m);
-        for (int i = 8; i < miin; i++) {
-            ans += freq[i] * 2;
-        }
-        miin = min(24, m);
-        for (int i = 16; i < miin; i++) {
-            ans += freq[i] * 3;
-        }
-        for (int i = 24; i < m; i++) {
-            ans += freq[i] * 4;
-        }
+
         return ans;
     }
 };
