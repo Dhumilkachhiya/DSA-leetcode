6ebcd78ee9ded71ee05c5c37201df960bcfaae0c
Dhumil Kachhiya <140485981+Dhumilkachhiya@users.noreply.github.com>
2026-08-05 16:02:51 +0530

    Time: 153 ms (92.41%), Space: 301.1 MB (81.03%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k,
+                                 vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+        for (auto& e : invocations) {
+            adj[e[0]].push_back(e[1]);
+        }
+        vector<int> vis(n, 0);
+        queue<int> q;
+
+        vis[k] = 1;
+        q.push(k);
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto nxt : adj[node]) {
+                if (!vis[nxt]) {
+                    vis[nxt] = 1;
+                    q.push(nxt);
+                }
+            }
+        }
+
+        for (auto& e : invocations) {
+            int u = e[0];
+            int v = e[1];
+
+            if (!vis[u] && vis[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i])
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
