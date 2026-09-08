11855be23f7b6a4dacaa4d450d995bd7a3507ee6
Dhumil Kachhiya <140485981+Dhumilkachhiya@users.noreply.github.com>
2026-09-08 13:57:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,18 +1,5 @@
 
 class Solution {
 public:
-    int countCommas(int n) {
-        int count = 0;
-        int temp = n;
-
-        while (temp > 0) {
-            count++;
-            temp /= 10;
-        }
-
-        if (count <= 3)
-            return 0;
-
-        return n - 999;
-    }
+    int countCommas(int n) { return n <= 999 ? 0 : n - 999; }
 };
